674e646609172f53c8a8a97b355bccef6c0f4a97
Brandon Pitts <brandomoki.bp@gmail.com>
2022-08-15 21:38:58 -0700

    created HornedBeast component

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,17 @@
 import React from "react";
+import HornedBeast from "./HornedBeast";
 
 class Main extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div>
-        
+        <HornedBeast title="Beast 1" imageUrl='https://via.placeholder.com/150/' description='description goes here'/>
+        <HornedBeast title="Beast 2" imageUrl='https://via.placeholder.com/150/' description='description goes here'/>
 
       </div>
     )
