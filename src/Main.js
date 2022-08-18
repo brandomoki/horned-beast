2a477f4ae60a70d254f22736b2e8eba6473d6112
Brandon Pitts <brandomoki.bp@gmail.com>
2022-08-17 23:26:27 -0700

    added module and logic but module doesnt render

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,26 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
 import Data from './data.json';
+import SelectedBeast from './SelectedBeast'
 
 class Main extends React.Component {
 
-
-  
+  constructor(props) {
+    super(props);
+    this.state = { show: false }
+  }
 
  
-
   
+  handleClose = () => {
+
+    this.setState( { show: false })
+   }
+
+  handleShow = () => {
+
+    this.setState( { show: true })
+   }
 
 
   render() {
@@ -17,9 +28,10 @@ class Main extends React.Component {
       <main>
         {
           Data.map(value => {
-          return <HornedBeast key={value._id} title={value.title} imageUrl={value.image_url} description={value.description}/>
+          return <HornedBeast key={value._id} open={this.handleShow} title={value.title} imageUrl={value.image_url} description={value.description}/>
         })
         }
+        <SelectedBeast />
         
 
       </main>
